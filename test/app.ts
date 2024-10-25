962efa0cd935a6db6c6359a796e87f040490814f
writedev <locxistwitch@gmail.com>
2024-10-25 20:37:30 +0200

    adding test file

---
+++ test/app.ts
@@ -0,0 +1,13 @@
+const w = 3;
+
+if (w === 3) {
+  console.log("w is a number");
+}
+
+const z = 5;
+
+if (w === null) {
+  console.log("w is null");
+} else if (w === Number.NaN) {
+  console.log("w is not a number");
+}
